894f321d8335202d38670c26d1256b1a4df68d2d
SENTHIL <123928930+hedge-hog-23@users.noreply.github.com>
2024-01-25 13:17:37 +0530

    Create 704. Binary Search.java

---
+++ 704. Binary Search.java
@@ -0,0 +1,63 @@
+// Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+
+// You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+// Example 1:
+
+// Input: nums = [-1,0,3,5,9,12], target = 9
+// Output: 4
+// Explanation: 9 exists in nums and its index is 4
+// Example 2:
+
+// Input: nums = [-1,0,3,5,9,12], target = 2
+// Output: -1
+// Explanation: 2 does not exist in nums so return -1
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 104
+// -104 < nums[i], target < 104
+// All the integers in nums are unique.
+// nums is sorted in ascending order.
+
+
+class Solution {
+    public int search(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length -1;
+        while(start<=end){
+            int mid = start + (end-start)/2; //equivalent to (start+end)/2 , avoids overfitting as int size (max is 2^32-1)
+            if(nums[mid] == target)
+                return mid;
+            else if (nums[mid]<target)
+                start = mid+1;
+            else
+                end = mid-1;
+          }
+          return -1;
+    }
+}
+
+// Accepted
+// SENTHIL
+// SENTHIL
+// submitted at Jan 25, 2024 13:15
+
+// Editorial
+
+// Solution
+// Runtime
+// 0
+// ms
+// Beats
+// 100.00%
+// of users with Java
+// Memory
+// 45.84
+// MB
+// Beats
+// 19.86%
+// of users with Java
