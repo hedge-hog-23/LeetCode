d505f9c0f70422d7893807e36898d97c796a2ab9
SENTHIL <123928930+hedge-hog-23@users.noreply.github.com>
2024-02-14 18:48:33 +0530

    Create 2149. Rearrange Array Elements by Sign.java

---
+++ 2149. Rearrange Array Elements by Sign.java
@@ -0,0 +1,69 @@
+// You are given a 0-indexed integer array nums of even length consisting of an equal number of positive and negative integers.
+
+// You should rearrange the elements of nums such that the modified array follows the given conditions:
+
+// Every consecutive pair of integers have opposite signs.
+// For all integers with the same sign, the order in which they were present in nums is preserved.
+// The rearranged array begins with a positive integer.
+// Return the modified array after rearranging the elements to satisfy the aforementioned conditions.
+
+ 
+
+// Example 1:
+
+// Input: nums = [3,1,-2,-5,2,-4]
+// Output: [3,-2,1,-5,2,-4]
+// Explanation:
+// The positive integers in nums are [3,1,2]. The negative integers are [-2,-5,-4].
+// The only possible way to rearrange them such that they satisfy all conditions is [3,-2,1,-5,2,-4].
+// Other ways such as [1,-2,2,-5,3,-4], [3,1,2,-2,-5,-4], [-2,3,-5,1,-4,2] are incorrect because they do not satisfy one or more conditions.  
+// Example 2:
+
+// Input: nums = [-1,1]
+// Output: [1,-1]
+// Explanation:
+// 1 is the only positive integer and -1 the only negative integer in nums.
+// So nums is rearranged to [1,-1].
+
+class Solution {
+    public int[] rearrangeArray(int[] nums) {
+        int n= nums.length;
+        int [] arr = new int[n];
+        int fst =0;
+        int snd = 1;
+        for(int i=0;i<n;i++){
+            if(nums[i]>0){
+                arr[fst] = nums[i]; 
+                fst+=2;
+            }
+            else{
+                arr[snd] = nums[i]; 
+                snd+=2;
+            }
+        }
+        
+        return arr;
+    }
+}
+
+
+// Accepted
+// SENTHIL
+// SENTHIL
+// submitted at Feb 14, 2024 18:42
+
+// Editorial
+
+// Solution
+// Runtime
+// 3
+// ms
+// Beats
+// 100.00%
+// of users with Java
+// Memory
+// 76.96
+// MB
+// Beats
+57.70%
+of users with Java
