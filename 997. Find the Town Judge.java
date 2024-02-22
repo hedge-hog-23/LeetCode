a774eab19f5a8d2d17da7204eb93173ee96f59d1
SENTHIL <123928930+hedge-hog-23@users.noreply.github.com>
2024-02-22 19:53:50 +0530

    Create 997. Find the Town Judge.java

---
+++ 997. Find the Town Judge.java
@@ -0,0 +1,76 @@
+// Easy
+// Topics
+// Companies
+// In a town, there are n people labeled from 1 to n. There is a rumor that one of these people is secretly the town judge.
+
+// If the town judge exists, then:
+
+// The town judge trusts nobody.
+// Everybody (except for the town judge) trusts the town judge.
+// There is exactly one person that satisfies properties 1 and 2.
+// You are given an array trust where trust[i] = [ai, bi] representing that the person labeled ai trusts the person labeled bi. If a trust relationship does not exist in trust array, then such a trust relationship does not exist.
+
+// Return the label of the town judge if the town judge exists and can be identified, or return -1 otherwise.
+
+ 
+
+// Example 1:
+
+// Input: n = 2, trust = [[1,2]]
+// Output: 2
+// Example 2:
+
+// Input: n = 3, trust = [[1,3],[2,3]]
+// Output: 3
+// Example 3:
+
+// Input: n = 3, trust = [[1,3],[2,3],[3,1]]
+// Output: -1
+ 
+
+// Constraints:
+
+// 1 <= n <= 1000
+// 0 <= trust.length <= 104
+// trust[i].length == 2
+// All the pairs of trust are unique.
+// ai != bi
+// 1 <= ai, bi <= n
+
+
+class Solution {
+    public int findJudge(int n, int[][] trust) {
+        int[] x = new int[n+1];   // trust someone 
+        int[] y = new int[n+1];  // trusted by someone
+System.out.println(trust.length);
+        for(int i=0;i<trust.length;i++){
+           int a = trust[i][0];    //can also use for-each 
+            int b = trust[i][1];
+            x[a]++;
+            y[b]++;
+        }
+        
+        
+        int ct =-1;
+        for(int i=1;i<=n;i++){
+            if(x[i] ==0 && y[i] == n-1){
+                ct = i;
+            }
+        }
+
+        return ct;
+    }
+}
+
+//explanation 
+
+// n = 3  => [1,3],[2,3]
+
+// x = [0,1,1,0]
+// y = [0,0,0,2]  0 == 2 //trust
+
+
+// n = 3 => [[1,3],[2,3],[3,1]]
+
+// x = [0,1,1,1]
+// y = [0,1,0,2]  // no trust 
