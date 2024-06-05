53fd3cb14ff5b520445ade29b76379e9229e2c21
SENTHIL <123928930+hedge-hog-23@users.noreply.github.com>
2024-06-05 19:42:58 +0530

    Create 1002. Find Common Characters.java

---
+++ 1002. Find Common Characters.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<String> commonChars(String[] words) {
+        int[] common = new int[26];
+        Arrays.fill(common,Integer.MAX_VALUE);
+        for(String s : words){
+            int[] ct = new int[26];
+            for(char c : s.toCharArray()){
+                ct[c-'a']++;
+            }
+            for(int i=0;i<26;i++){
+                common[i] = Math.min(common[i],ct[i]);
+            }
+        }
+        List<String> ans = new ArrayList<>();
+        for(int i=0;i<=25;i++){
+            for(int j=0;j<common[i];j++){
+                
+                ans.add("" + (char)('a' + i));
+            }
+        }
+        return ans;
+
+        
+
+    }
+}
