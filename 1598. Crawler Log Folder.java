22435cc833314710b23cb51a4a9ed57b7c09640c
SENTHIL <123928930+hedge-hog-23@users.noreply.github.com>
2024-07-10 22:00:37 +0530

    Update 1598. Crawler Log Folder.java

--- 1598. Crawler Log Folder.java
+++ 1598. Crawler Log Folder.java
@@ -2,7 +2,8 @@ class Solution {
     public int minOperations(String[] logs) {
         Stack<String> s = new Stack<>();
         for(String c : logs){
-            if(c.equals("../") && !(s.isEmpty())){
+            if(c.equals("../")){
+                if(!(s.isEmpty()))
                 s.pop();
             }
             else if(!(c.equals("./"))){
