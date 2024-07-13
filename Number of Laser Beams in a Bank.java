ee06b8db7cf169e53c5c4b6cb131858a16d7b48c
SENTHIL <123928930+hedge-hog-23@users.noreply.github.com>
2024-07-13 21:31:13 +0530

    Create Number of Laser Beams in a Bank.java

---
+++ Number of Laser Beams in a Bank.java
@@ -0,0 +1,12 @@
+class Solution:
+    def numberOfBeams(self, bank: List[str]) -> int:
+        ans = 0
+        temp = 0
+        for i in bank:
+            n = s.count('1')
+            if(n == 0):
+                continue
+            ans = ans + temp * n
+            temp = n
+
+        return ans
