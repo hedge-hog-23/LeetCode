4f452d99311eb2aea327fd8e8273084dcf6a82b3
SENTHIL <123928930+hedge-hog-23@users.noreply.github.com>
2024-08-09 22:36:24 +0530

    Create 3190. Find Minimum Operations to Make All Elements Divisible by Three.py

---
+++ 3190. Find Minimum Operations to Make All Elements Divisible by Three.py
@@ -0,0 +1,8 @@
+class Solution:
+    def minimumOperations(self, nums: List[int]) -> int:
+        op = 0
+        for i in range(0,len(nums)):
+            if(nums[i]%3 !=0):
+                op+=1
+        return op
+        
