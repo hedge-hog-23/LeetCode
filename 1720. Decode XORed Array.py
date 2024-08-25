ff5a057e03d271ba4edfc857e9018f81adea4e52
SENTHIL <123928930+hedge-hog-23@users.noreply.github.com>
2024-08-25 21:44:37 +0530

    Create 1720. Decode XORed Array.py

---
+++ 1720. Decode XORed Array.py
@@ -0,0 +1,6 @@
+class Solution:
+    def decode(self, encoded: List[int], first: int) -> List[int]:
+        res= [first]
+        for i in range(0,len(encoded)):
+            res.append(res[-1] ^ encoded[i])
+        return res
